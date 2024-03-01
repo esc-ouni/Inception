bc04477a2b09f8ac1b5cbfe0d8fd3aa6c0742a7d
esc-ouni <idouni@student.1337.ma>
2024-03-01 19:31:27 +0100

    Ideal_world

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp 
-
-wp core download --allow-root
-
-wp core config create --dbname=your_database_name --dbuser=your_database_user --dbpass=your_database_password --dbhost=localhost --dbprefix=wp_ --allow-root
-
-wp core install --url=your_site_url --title="Your Site Title" --admin_user=admin_username --admin_password=admin_password --admin_email=admin_email@example.com --allow-root
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
-
-bash php-fpm8.2 -F
+wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
