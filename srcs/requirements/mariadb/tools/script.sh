037fe97b13f9fff5b471f3b23176dd2bb7badd97
esc-ouni <idouni@student.1337.ma>
2024-03-02 18:22:49 +0100

    Wordpress_C_TBC

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,6 @@
+
+
+# sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+
+# mariadbd-safe &
--- srcs/requirements/wordpress/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
