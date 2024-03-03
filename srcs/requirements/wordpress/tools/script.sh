43e2f8fc83979b829fafed54e9417ccf8dce36b2
esc-ouni <idouni@student.1337.ma>
2024-03-03 18:42:02 +0100

    Basiis

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-wp config create	--allow-root \
-                    --dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
-                    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+sed -i 's/\;clear_env = no/clear_env = no/g' /etc/php/8.2/fpm/pool.d/www.conf
+
+echo "\nlisten=wordpress:9000" >> /etc/php/8.2/fpm/pool.d/www.conf
+
+
+# wp config create	--allow-root \
+#                     --dbname=$SQL_DATABASE \
+#                     --dbuser=$SQL_USER \
+#                     --dbpass=$SQL_PASSWORD \
+#                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
 					
