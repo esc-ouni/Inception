4df49b506b80f1e620dc6357acaf395eb4cca15a
esc-ouni <idouni@student.1337.ma>
2024-03-03 19:57:46 +0100

    COPY_prblm

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+unzip -q ./wordpress-6.4.3.zip -d ./var/www
+
+cp -rf /var/www/wordpress/* /var/www/html/. #WHYYY
+
 sed -i 's/\;clear_env = no/clear_env = no/g' /etc/php/8.2/fpm/pool.d/www.conf
 
 echo "\nlisten=wordpress:9000" >> /etc/php/8.2/fpm/pool.d/www.conf
 
+rm -rf wordpress-6.4.3.zip
+
+rm -rf /tmp/*
 
 # wp config create	--allow-root \
 #                     --dbname=$SQL_DATABASE \
