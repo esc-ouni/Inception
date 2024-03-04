d45f545d692ca8e6ed47c13e4f3541d3bbb2efa5
esc-ouni <idouni@student.1337.ma>
2024-03-04 10:46:24 +0100

    WP_config

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -12,11 +12,11 @@ echo "\nlisten=wordpress:9000" >> /etc/php/8.2/fpm/pool.d/www.conf
 
 sleep 10
 
-wp config create	--allow-root \
-                    --dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
-                    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+# wp config create	--allow-root \
+#                     --dbname=$SQL_DATABASE \
+#                     --dbuser=$SQL_USER \
+#                     --dbpass=$SQL_PASSWORD \
+#                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 rm -rf /tmp/*
 
