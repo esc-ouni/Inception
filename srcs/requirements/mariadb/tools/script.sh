10a25d7bf59a0f604bd10791f05d6eda905b6612
esc-ouni <idouni@student.1337.ma>
2024-03-04 21:17:00 +0100

    Engoing_FOI

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-mysqld_safe &
+service mariadb start
 
-mariadb -u root <<EOF
-CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO $MYSQL_USER@'%';
-FLUSH PRIVILEGES;
-EOF
+mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
-# mysqld_safe
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+
+mysql -e "FLUSH PRIVILEGES;"
 
 echo "=> DATA_BASEEEEEE"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,7 +8,7 @@ sed -i 's/\;clear_env = no/clear_env = no/g' /etc/php/8.2/fpm/pool.d/www.conf
 
 echo "\nlisten=wordpress:9000" >> /etc/php/8.2/fpm/pool.d/www.conf
 
-# mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php #testt
+# cp -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php #when_db_ready
 
 # dBase
 
@@ -16,15 +16,17 @@ sleep 10
 
 cd /var/www/html
 
-# wp core download --allow-root 
-# mv wp-config-sample.php wp-config.php 
+wp core download --allow-root 
+
+cp wp-config-sample.php wp-config.php
+
 # wp config set SERVER_PORT 3306 --allow-root
 # wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html
 # wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html
 # wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html
 # wp config set DB_HOST 'mariadb:3306' --allow-root --path=/var/www/html
-# wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD --admin_email="eamil.email" --allow-root --path=/var/www/html
-# wp user create $WORDPRESS_DB_USER $WP_USER_EMAIL --role=author --user_pass=$WORDPRESS_DB_PASSWORD --allow-root --path=/var/www/html
+# wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=email --allow-root --path=/var/www/html
+# wp user create $MYSQL_USER email --role=author --user_pass=$MYSQL_PASSWORD --allow-root --path=/var/www/html
 
 rm -rf /tmp/*
 
