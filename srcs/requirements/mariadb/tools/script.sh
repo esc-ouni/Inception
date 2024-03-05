a23de360e3927cd3d4dae252b6b1aa9bb1183f28
esc-ouni <idouni@student.1337.ma>
2024-03-05 14:30:46 +0100

    Engoing_FOI

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 service mariadb start
-
-mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
-
-mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-
-mysql -e "FLUSH PRIVILEGES;"
-
+sleep 2
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mariadb -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$MYSQL_PASSWORD shutdown
 echo "=> DATA_BASEEEEEE"
+
+# mariadbd
+mysqld_safe
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-unzip -q /tmp/wordpress-6.4.3.zip -d /var/www
+# unzip -q /tmp/wordpress-6.4.3.zip -d /var/www
 
-cp -rf /var/www/wordpress/* /var/www/html/. #WHYYY
+# cp -rf /var/www/wordpress/* /var/www/html/. #WHYYY
 
 sed -i 's/\;clear_env = no/clear_env = no/g' /etc/php/8.2/fpm/pool.d/www.conf
 
@@ -18,16 +18,18 @@ cd /var/www/html
 
 wp core download --allow-root 
 
-cp wp-config-sample.php wp-config.php
+# cp wp-config-sample.php wp-config.php
 
 # wp config set SERVER_PORT 3306 --allow-root
 # wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html
 # wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html
 # wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html
 # wp config set DB_HOST 'mariadb:3306' --allow-root --path=/var/www/html
-# wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=email --allow-root --path=/var/www/html
-# wp user create $MYSQL_USER email --role=author --user_pass=$MYSQL_PASSWORD --allow-root --path=/var/www/html
+wp config  --allow-root  create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
+wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=email --allow-root --path=/var/www/html
+wp user create $MYSQL_USER email --role=author --user_pass=$MYSQL_PASSWORD --allow-root --path=/var/www/html
 
-rm -rf /tmp/*
+# rm -rf /tmp/*
 
+php-fpm8.2 -F
 					
