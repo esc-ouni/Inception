e82eee1af762714bd7b925582d664d911d978155
esc-ouni <idouni@student.1337.ma>
2024-03-06 11:40:03 +0100

    Engoing_FOI

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,8 +4,6 @@ sed -i 's/\;clear_env = no/clear_env = no/g' /etc/php/8.2/fpm/pool.d/www.conf
 
 echo "\nlisten=wordpress:9000" >> /etc/php/8.2/fpm/pool.d/www.conf
 
-sleep 10
-
 wp core download --locale=en_US --allow-root 
 
 wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
@@ -15,6 +13,3 @@ wp core install --allow-root --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$
 wp user create --allow-root $MYSQL_USER email --role=author --user_pass=$MYSQL_PASSWORD --path=/var/www/html
 
 php-fpm8.2 -F
-
-sleep 10
-					
