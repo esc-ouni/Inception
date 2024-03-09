250fada75dc863a3a4648f13af9bf3fc2e37fcd3
esc-ouni <idouni@student.1337.ma>
2024-03-09 19:48:03 +0100

    Engoing_FOI_PRGS

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,12 +6,10 @@ sed -i 's!listen = /run/php/php8.2-fpm.sock!listen=wordpress:9000!g' /etc/php/8.
 
 wp core download --locale=en_US --allow-root 
 
-wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --allow-root
 
-wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$ADMIN_USR --admin_password=$ADMIN_PSW --admin_email=$ADMIN_EMAIL --allow-root --path=/var/www/html
+wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$ADMIN_USR --admin_password=$ADMIN_PSW --admin_email=$ADMIN_EMAIL --allow-root
 
-# wp user create $USER_USR $USER_EMAIL --role=author --user_pass=$USER_PSW --allow-root --path=/var/www/html
-
-wp user create $USER_USR $USER_EMAIL --role=Bios --user_pass=$USER_PSW --allow-root --path=/var/www/html
+wp user create $USER_USR $USER_EMAIL --role=author --user_pass=$USER_PSW --allow-root
 
 php-fpm8.2 -F
